53f6ace1db30eb3d8bd198182ef7337caa29819f
icebowl <icebowl1967@gmail.com>
2020-01-31 04:28:35 -0800

    AA

--- camera/cam-file-0.py
+++ camera/cam-file-0.py
@@ -1,21 +1,14 @@
 #65536.py cwc
-import random
-import datetime
+from datetime import datetime
 from picamera import PiCamera
 from time import sleep
 
 def getDateString():
-	dt = datetime.datetime.now()
-	y = dt.year - 2000 
-	hr = dt.hour
-	if hr < 10:
-		hr = "0" + str(hr)
-	me = dt.minute
-	if me < 10:
-		me = "0" + str(me)
-	timestring = str(y) + str(dt.month) + str(dt.day)+"-"+str(hr)+str(me)+"."+str(dt.second)
-	return timestring
-
+	now =datetime.now()
+	nowYear = str(now.year);nowSeconds = str(now.second)
+	dateTimeString =  nowYear[2:4]+str(now.month)+str(now.day)+"-"+str(now.hour)+"-"+str(now.minute)+"-"+nowSeconds[0:2]
+	print(dateTimeString)
+	return dateTimeString
 def camPict():
 	filename = getDateString()
 	filename = filename + ".jpg"	
@@ -24,7 +17,7 @@ def camPict():
 	camera.rotation = 180
 	camera.start_preview()
 	# Camera warm-up time
-	sleep(1)
+	#sleep(1)
 	camera.capture(filename)
 	camera.stop_preview()
 	 
@@ -34,4 +27,3 @@ def main():
 if __name__ == '__main__':
 	main()
 
-#python 65536.py > 191213-1302.txt
